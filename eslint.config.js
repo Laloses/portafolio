98ecf84c5f5046ad18b833a4e0c963fe08a078ab
Laloses <laloses_eduardo@hotmail.com>
2024-04-14 17:35:08 -0600

    Added rule ESlint types

--- eslint.config.js
+++ eslint.config.js
@@ -4,53 +4,56 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import eslint from '@eslint/js';
 
 // New Flat configs of ESlint
-export default tsESlint.config(
-  eslint.configs.recommended,
-  ...tsESlint.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: {
-        browser: true,
+export default tsESlint.config({
+  files: ['src/**/*.ts', 'src/**/*.tsx'],
+  extends: [eslint.configs.recommended, ...tsESlint.configs.recommended],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: {
+      browser: true,
+    },
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  plugins: {
+    'react-refresh': reactRefresh,
+    'react-hooks': reactHooks,
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: '@mui/material', //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
+            message: 'Use imports from @mui/material/<component>',
+          },
+          {
+            name: '@mui/icons-material', //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
+            message: 'Use imports from @mui/icons-material/<icon>',
+          },
+        ],
+        patterns: [
+          {
+            group: ['@mui/*/*/*'], //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
+            message:
+              'Material UI only support first and second-level imports. Use imports from @mui/material/<component>',
+          },
+          {
+            group: ['@mui/icons-material/*/*'], //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
+            message:
+              'Material UI only support first and second-level imports. Use imports from @mui/icons-material/<icon>',
+          },
+        ],
       },
-    },
-    plugins: {
-      'react-refresh': reactRefresh,
-      'react-hooks': reactHooks,
-    },
-    rules: {
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'no-restricted-imports': [
-        'error',
-        {
-          paths: [
-            {
-              name: '@mui/material', //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
-              message: 'Use imports from @mui/material/<component>',
-            },
-            {
-              name: '@mui/icons-material', //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
-              message: 'Use imports from @mui/icons-material/<icon>',
-            },
-          ],
-          patterns: [
-            {
-              group: ['@mui/*/*/*'], //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
-              message:
-                'Material UI only support first and second-level imports. Use imports from @mui/material/<component>',
-            },
-            {
-              group: ['@mui/icons-material/*/*'], //https://mui.com/material-ui/guides/minimizing-bundle-size/#option-one-use-path-imports
-              message:
-                'Material UI only support first and second-level imports. Use imports from @mui/icons-material/<icon>',
-            },
-          ],
-        },
-      ],
-    },
-  }
-);
+    ],
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+});
